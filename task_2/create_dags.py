d52676b29df6cb984d2ad032d421e325ba366b6b
Charles <charles.laurioz@gmail.com>
2023-10-20 12:57:35 +0200

    nouveaux dags

---
+++ task_2/create_dags.py
@@ -0,0 +1,29 @@
+import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.decorators import task_group
+import json
+
+BASE_DIR = "/opt/airflow/dags"
+dependancy_graph = json.load(open(BASE_DIR + "/task_1/data.json"))
+
+
+def create_dag(model, deps):
+
+    dag = DAG(
+        dag_id=model,
+        start_date=datetime.datetime(2023,10,18),
+        #schedule="@daily",
+    )
+
+    model_task = EmptyOperator(model, dag=dag)
+    for dep in deps:
+        etl_task = EmptyOperator(dep, dag=dag)
+        etl_task >> model_task
+ 
+
+for model in dependancy_graph:
+    create_dag(model, dependancy_graph[model])
+    
